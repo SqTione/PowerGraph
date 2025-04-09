1f71a5cce9170347b05f74a5fc15fd011162171a
SqTione <sqtione@icloud.com>
2025-04-09 18:54:47 +0300

    Merged develop into main

--- src/protected/config/database.php
+++ src/protected/config/database.php
@@ -1,11 +1,17 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../components/EnvHelper.php');
+
+$dbname = EnvHelper::getEnv('MYSQL_DATABASE');
+$db_username = EnvHelper::getEnv('MYSQL_USER');
+$db_password = EnvHelper::getEnv('MYSQL_PASSWORD');
+
 // This is the database connection configuration.
 return array(
     'class'=>'CDbConnection',
-    'connectionString'=>'mysql:host=db;dbname=power_graph_db',
-    'username'=>'user',
-    'password'=>'user_password',
+    'connectionString'=>'mysql:host=db;dbname=' . $dbname,
+    'username'=>$db_username,
+    'password'=>$db_password,
     'emulatePrepare'=>true,
     'charset'=>'utf8',
 );
